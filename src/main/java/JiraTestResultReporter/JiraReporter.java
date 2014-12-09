757ad878bc5705b75c43151e30681bfa8179d849
Luigi Lentini <luigi.lentini@here.com>
2014-12-09 18:11:13 +0100

    Adding the proper headers to HTTP requests + cleanup

--- src/main/java/JiraTestResultReporter/JiraReporter.java
+++ src/main/java/JiraTestResultReporter/JiraReporter.java
@@ -1,58 +1,53 @@
 package JiraTestResultReporter;
 
-import hudson.Launcher;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 import hudson.Extension;
 import hudson.FilePath;
+import hudson.Launcher;
 import hudson.model.AbstractBuild;
-import hudson.model.BuildListener;
 import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
 import hudson.tasks.junit.CaseResult;
 import hudson.tasks.junit.TestResultAction;
-import hudson.tasks.test.AbstractTestResultAction;
 import hudson.util.FormValidation;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
-import org.json.JSONObject;
-import org.json.JSONArray;
-
-import com.mashape.unirest.request.HttpRequestWithBody;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-
-import java.io.IOException;
 import java.io.PrintStream;
-import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
 public class JiraReporter extends Notifier {
 
-    public String projectKey;
-    public String serverAddress;
-    public String username;
-    public String password;
-
-    public boolean debugFlag;
-    public boolean verboseDebugFlag;
-    public boolean createAllFlag;
-
-    private transient FilePath workspace;
-
+    //private static final Integer[] JIRA_SUCCESS_CODES = {201, 200};
+    private static final int JIRA_CREATED_CODE = 201;
     private static final int JIRA_SUCCESS_CODE = 200;
-
     private static final String PluginName = new String("[JiraTestResultReporter]");
     private final String pInfo = String.format("%s [INFO]", PluginName);
     private final String pDebug = String.format("%s [DEBUG]", PluginName);
     private final String pVerbose = String.format("%s [DEBUGVERBOSE]", PluginName);
     private final String prefixError = String.format("%s [ERROR]", PluginName);
+    public String projectKey;
+    public String serverAddress;
+    public String username;
+    public String password;
+    public boolean debugFlag;
+    public boolean verboseDebugFlag;
+    public boolean createAllFlag;
+    private transient FilePath workspace;
 
     @DataBoundConstructor
     public JiraReporter(String projectKey,
@@ -102,7 +97,7 @@ public class JiraReporter extends Notifier {
         debugLog(listener,
                  String.format("%s Workspace is %s%n", pInfo, this.workspace.toString())
                 );
-        //AbstractTestResultAction<?> testResultAction = build.getAction(AbstractTestResultAction.class);
+
         TestResultAction testResultAction = build.getAction(TestResultAction.class);
         if (testResultAction == null) {
             logger.printf("%s no test results found; nothing to do.%n", pInfo);
@@ -153,22 +148,21 @@ public class JiraReporter extends Notifier {
      void createJiraIssue(final List<CaseResult> failedTests,
                           final AbstractBuild build,
                           final BuildListener listener) {
-        PrintStream logger = listener.getLogger();
-        String jiraAPIUrl = this.serverAddress + "rest/api/latest/";
+         PrintStream logger = listener.getLogger();
+         String jiraAPIUrl = this.serverAddress + "rest/api/latest/";
          String jiraAPIUrlSearch = jiraAPIUrl + "search";
          String jiraAPIUrlIssue = jiraAPIUrl + "issue";
 
-        for (CaseResult result : failedTests) {
+        for (CaseResult failedTest : failedTests) {
             try {
                 // check for tickets in JIRA
                 // GET search
-                // TODO: change to a POST request
+                // TODO: change to a POST request?
                 HttpResponse<JsonNode> jsonResponse = Unirest.get(jiraAPIUrlSearch)
                         //.header("accept", "application/json")
-                        //.field("jql", "project = TEST AND status in (Open, \"In Progress\") AND reporter in (lentini)")
                         .basicAuth(this.username, this.password)
-                        .queryString("jql", "project = " + this.projectKey +  " AND status in (Open, \"In Progress\") " +
-                                "AND reporter in (" + this.username + ") AND summary ~ \"Test " + result.getName() + " failed\"")
+                        .queryString("jql", "project = " + this.projectKey + " AND status in (Open, \"In Progress\") " +
+                                "AND reporter in (" + this.username + ") AND summary ~ \"Test " + failedTest.getName() + " failed\"")
                         .queryString("fields", "summary")
                         .asJson();
 
@@ -182,7 +176,7 @@ public class JiraReporter extends Notifier {
                 );
 
                 if (jsonResponse.getStatus() != JIRA_SUCCESS_CODE) {
-                    throw new RuntimeException(this.prefixError + " Failed : HTTP error code : " + jsonResponse.getStatus());
+                    throw new RuntimeException(this.prefixError + " Failed while searching for issues: HTTP error code : " + jsonResponse.getStatus());
                 }
 
                 logger.printf("%s Existing tickets found: %d\n", pVerbose, jsonResponse.getBody().getObject().getInt("total"));
@@ -192,70 +186,75 @@ public class JiraReporter extends Notifier {
                     JSONArray issues = jsonResponse.getBody().getObject().getJSONArray("issues");
 
                     //for (int i=0; issues.length(); ++i) {
-                        String jiraAPIUrlIssueComment = jiraAPIUrlIssue + "/" + issues.getJSONObject(0).getString("key") + "/comment";
+                    String jiraAPIUrlIssueComment = jiraAPIUrlIssue + "/" + issues.getJSONObject(0).getString("key") + "/comment";
 
-                        HttpResponse<JsonNode> createIssueResponse = Unirest.post(jiraAPIUrlIssueComment)
-                                .header("accept", "application/json")
-                                .basicAuth(this.username, this.password)
-                                //.body(jsonNodeFields)
-                                .asJson();
-                    //}
-                }
-                else {
-                    // create a new ticket
-                    // ticket fields
-                    String summary = "Test " + result.getName() + " failed";
-                    String description = "Test class: " + result.getClassName() + "\n\n" +
-                                            // TODO: fix this call
-                                            //"Jenkins job: " + build.getAbsoluteUrl() + "\n\n" +
-                                            "{noformat}\n" + result.getErrorDetails() + "\n{noformat}\n\n" +
-                                            "{noformat}\n" + result.getErrorStackTrace().replace(this.workspace.toString(), "") +
-                                            "\n{noformat}\n\n";
-
-                    // create a JSON structure out of the fields
-                    JSONObject jsonSubFields = new JSONObject();
-                    JSONObject projectFields = new JSONObject();
-                    JSONObject issueTypeFields = new JSONObject();
-                    JSONObject jsonFields = new JSONObject();
-                    projectFields.put("key", this.projectKey);
-                    issueTypeFields.put("name", "Bug");
-                    jsonSubFields.put("summary", summary).put("description", description).put("project", projectFields)
-                                .put("issuetype", issueTypeFields);
-                    jsonFields.put("fields", jsonSubFields);
-                    JsonNode jsonNodeFields = new JsonNode(jsonFields.toString());
-
-                    /*HttpRequestWithBody request = Unirest.post(jiraAPIUrlIssue);
-                    request.body(jsonFields.toString());*/
-
-                    // make POST issue request
-                    //TODO: POST is not working yet
-                    // JIRA returns the exception
-                    // java.lang.RuntimeException: java.lang.RuntimeException: org.json.JSONException:
-                    // A JSONArray text must start with '[' at 1 [character 2 line 1]
-                    HttpResponse<JsonNode> createIssueResponse = Unirest.post(jiraAPIUrlIssue)
-                            .header("accept", "application/json")
+                    HttpResponse<JsonNode> addCommentResponse = Unirest.post(jiraAPIUrlIssueComment)
+                            //.header("accept", "application/json")
+                            .header("content-type", "application/json")
                             .basicAuth(this.username, this.password)
-                            .body(jsonNodeFields)
+                            .body("{\"body\": \"The same test has failed again in " + build.getAbsoluteUrl() + ".\"}")
                             .asJson();
 
                     debugLog(listener,
                             String.format("statusLine: %s%n",
-                                    createIssueResponse.getStatusText())
+                                    addCommentResponse.getStatusText())
                     );
                     debugLog(listener,
                             String.format("statusCode: %d%n",
-                                    createIssueResponse.getStatus())
+                                    addCommentResponse.getStatus())
                     );
 
-                    if (createIssueResponse.getStatus() != JIRA_SUCCESS_CODE) {
-                        throw new RuntimeException(this.prefixError + " Failed : HTTP error code : " + createIssueResponse.getStatus());
+                    if (addCommentResponse.getStatus() != JIRA_CREATED_CODE) {
+                        throw new UnirestException(this.prefixError + " Failed while adding a comment: HTTP error code : " + addCommentResponse.getStatus());
                     }
                 }
+                else {
+                        // create a new ticket
+                        // ticket fields
+                        String summary = "Test " + failedTest.getName() + " failed";
+                        String description = "Test class: " + failedTest.getClassName() + "\n\n" +
+                                "Jenkins job: " + build.getAbsoluteUrl() + "\n\n" +
+                                "{noformat}\n" + failedTest.getErrorDetails() + "\n{noformat}\n\n" +
+                                "{noformat}\n" + failedTest.getErrorStackTrace().replace(this.workspace.toString(), "") +
+                                "\n{noformat}\n\n";
 
-                //Unirest.shutdown();
+                        // create a JSON structure out of the fields
+                        JSONObject jsonSubFields = new JSONObject();
+                        JSONObject projectFields = new JSONObject();
+                        JSONObject issueTypeFields = new JSONObject();
+                        JSONObject jsonFields = new JSONObject();
+                        projectFields.put("key", this.projectKey);
+                        issueTypeFields.put("name", "Bug");
+                        jsonSubFields.put("summary", summary).put("description", description).put("project", projectFields)
+                                .put("issuetype", issueTypeFields);
+                        jsonFields.put("fields", jsonSubFields);
+                        JsonNode jsonNodeFields = new JsonNode(jsonFields.toString());
+
+                        // make POST issue request
+                        HttpResponse<JsonNode> createIssueResponse = Unirest.post(jiraAPIUrlIssue)
+                                .header("accept", "application/json")
+                                .header("content-type", "application/json")
+                                .basicAuth(this.username, this.password)
+                                .body(jsonNodeFields)
+                                .asJson();
+
+                        debugLog(listener,
+                                String.format("statusLine: %s%n",
+                                        createIssueResponse.getStatusText())
+                        );
+                        debugLog(listener,
+                                String.format("statusCode: %d%n",
+                                        createIssueResponse.getStatus())
+                        );
+
+                        if (createIssueResponse.getStatus() != JIRA_CREATED_CODE) {
+                            throw new UnirestException(this.prefixError + " Failed while creating an issue: HTTP error code : " + createIssueResponse.getStatus());
+                        }
+                }
+                    //Unirest.shutdown();
             }
             catch (UnirestException e) {
-                logger.printf("%s Problems found while connecting to JIRA: %s\n", prefixError, e.toString());
+                logger.printf("%s\n", e.getMessage());
                 e.printStackTrace();
             }
 /*            catch (MalformedURLException e) {
